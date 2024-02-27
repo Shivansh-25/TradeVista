bc7af1dfbbf0be468d8e36bc26bb966e5c77a994
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2024-02-28 03:44:08 +0530

    Update dependencies and added the box for Top Gainers and Top Losers

--- trade_vista/app/page.js
+++ trade_vista/app/page.js
@@ -1,15 +1,32 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import TradingViewWidget from "./Components/TradingViewWidget";
+
 import React from "react";
+import TopGainers from "./Components/TopGainers";
+import GrayBox from "./Components/GrayBox";
+import TopLosers from "./Components/TopLosers";
 
 export default function Home() {
+  const [stock, setStock] = useState("");
+
+  useEffect(() => {
+    seeStockDetail(stock);
+  }, [stock]);
+
+  const seeStockDetail = (symbol) => {
+    setStock(symbol);
+  };
+
   return (
     <>
       <div className="w-screen h-screen">
-        <div style={{height: "60vh", width: "60vw"}}>
+        <div style={{ height: "60vh", width: "70vw" }}>
           <TradingViewWidget />
         </div>
+        <GrayBox component={TopGainers} text={"Top Gainers"} />
+        <GrayBox component={TopLosers} text={"Top Losers"} />
       </div>
     </>
   );
