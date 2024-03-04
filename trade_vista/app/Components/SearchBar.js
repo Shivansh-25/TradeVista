9578e6526fe27c0682af0c9093493f175e30c45b
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2024-03-04 23:24:43 +0530

    Added API Call to the Search Bar

--- trade_vista/app/Components/SearchBar.js
+++ trade_vista/app/Components/SearchBar.js
@@ -1,134 +1,153 @@
 import React, { useState } from "react";
 
-const SearchBar = () => {
-  const sampleData = {
-    bestMatches: [
-      {
-        "1. symbol": "BA",
-        "2. name": "Boeing Company",
-        "3. type": "Equity",
-        "4. region": "United States",
-        "5. marketOpen": "09:30",
-        "6. marketClose": "16:00",
-        "7. timezone": "UTC-04",
-        "8. currency": "USD",
-        "9. matchScore": "1.0000",
-      },
-      {
-        "1. symbol": "BAB",
-        "2. name": "INVESCO TAXABLE MUNICIPAL BOND ETF ",
-        "3. type": "ETF",
-        "4. region": "United States",
-        "5. marketOpen": "09:30",
-        "6. marketClose": "16:00",
-        "7. timezone": "UTC-04",
-        "8. currency": "USD",
-        "9. matchScore": "0.8000",
-      },
-      {
-        "1. symbol": "BA.LON",
-        "2. name": "BAE Systems plc",
-        "3. type": "Equity",
-        "4. region": "United Kingdom",
-        "5. marketOpen": "08:00",
-        "6. marketClose": "16:30",
-        "7. timezone": "UTC+01",
-        "8. currency": "GBX",
-        "9. matchScore": "0.6667",
-      },
-      {
-        "1. symbol": "BABA",
-        "2. name": "Alibaba Group Holding Ltd",
-        "3. type": "Equity",
-        "4. region": "United States",
-        "5. marketOpen": "09:30",
-        "6. marketClose": "16:00",
-        "7. timezone": "UTC-04",
-        "8. currency": "USD",
-        "9. matchScore": "0.6667",
-      },
-      {
-        "1. symbol": "BA3.FRK",
-        "2. name": "Brooks Automation Inc",
-        "3. type": "Equity",
-        "4. region": "Frankfurt",
-        "5. marketOpen": "08:00",
-        "6. marketClose": "20:00",
-        "7. timezone": "UTC+02",
-        "8. currency": "EUR",
-        "9. matchScore": "0.5714",
-      },
-      {
-        "1. symbol": "BAAPX",
-        "2. name": "BlackRock Aggressive GwthPrprdPtfInvstrA",
-        "3. type": "Mutual Fund",
-        "4. region": "United States",
-        "5. marketOpen": "09:30",
-        "6. marketClose": "16:00",
-        "7. timezone": "UTC-04",
-        "8. currency": "USD",
-        "9. matchScore": "0.5714",
-      },
-      {
-        "1. symbol": "BAAAAX",
-        "2. name": "Building America Strategy Port CDA USD Ser 21/1Q MNT CASH",
-        "3. type": "Mutual Fund",
-        "4. region": "United States",
-        "5. marketOpen": "09:30",
-        "6. marketClose": "16:00",
-        "7. timezone": "UTC-04",
-        "8. currency": "USD",
-        "9. matchScore": "0.5000",
-      },
-      {
-        "1. symbol": "BAAAFX",
-        "2. name": "Building America Strgy Portf CDA USD Ser 2022/2Q MNT CASH",
-        "3. type": "Mutual Fund",
-        "4. region": "United States",
-        "5. marketOpen": "09:30",
-        "6. marketClose": "16:00",
-        "7. timezone": "UTC-04",
-        "8. currency": "USD",
-        "9. matchScore": "0.5000",
-      },
-      {
-        "1. symbol": "BAB3.LON",
-        "2. name": "Leverage Shares 3x Alibaba ETP Securities",
-        "3. type": "ETF",
-        "4. region": "United Kingdom",
-        "5. marketOpen": "08:00",
-        "6. marketClose": "16:30",
-        "7. timezone": "UTC+01",
-        "8. currency": "USD",
-        "9. matchScore": "0.5000",
-      },
-      {
-        "1. symbol": "BAAX39.SAO",
-        "2. name": "Ishares Msci All Country Asia Ex Japan ETF",
-        "3. type": "ETF",
-        "4. region": "Brazil/Sao Paolo",
-        "5. marketOpen": "10:00",
-        "6. marketClose": "17:30",
-        "7. timezone": "UTC-03",
-        "8. currency": "BRL",
-        "9. matchScore": "0.3636",
-      },
-    ],
-  };
+const sampleData = {
+  bestMatches: [
+    {
+      "1. symbol": "BA",
+      "2. name": "Boeing Company",
+      "3. type": "Equity",
+      "4. region": "United States",
+      "5. marketOpen": "09:30",
+      "6. marketClose": "16:00",
+      "7. timezone": "UTC-04",
+      "8. currency": "USD",
+      "9. matchScore": "1.0000",
+    },
+    {
+      "1. symbol": "BAB",
+      "2. name": "INVESCO TAXABLE MUNICIPAL BOND ETF ",
+      "3. type": "ETF",
+      "4. region": "United States",
+      "5. marketOpen": "09:30",
+      "6. marketClose": "16:00",
+      "7. timezone": "UTC-04",
+      "8. currency": "USD",
+      "9. matchScore": "0.8000",
+    },
+    {
+      "1. symbol": "BA.LON",
+      "2. name": "BAE Systems plc",
+      "3. type": "Equity",
+      "4. region": "United Kingdom",
+      "5. marketOpen": "08:00",
+      "6. marketClose": "16:30",
+      "7. timezone": "UTC+01",
+      "8. currency": "GBX",
+      "9. matchScore": "0.6667",
+    },
+    {
+      "1. symbol": "BABA",
+      "2. name": "Alibaba Group Holding Ltd",
+      "3. type": "Equity",
+      "4. region": "United States",
+      "5. marketOpen": "09:30",
+      "6. marketClose": "16:00",
+      "7. timezone": "UTC-04",
+      "8. currency": "USD",
+      "9. matchScore": "0.6667",
+    },
+    {
+      "1. symbol": "BA3.FRK",
+      "2. name": "Brooks Automation Inc",
+      "3. type": "Equity",
+      "4. region": "Frankfurt",
+      "5. marketOpen": "08:00",
+      "6. marketClose": "20:00",
+      "7. timezone": "UTC+02",
+      "8. currency": "EUR",
+      "9. matchScore": "0.5714",
+    },
+    {
+      "1. symbol": "BAAPX",
+      "2. name": "BlackRock Aggressive GwthPrprdPtfInvstrA",
+      "3. type": "Mutual Fund",
+      "4. region": "United States",
+      "5. marketOpen": "09:30",
+      "6. marketClose": "16:00",
+      "7. timezone": "UTC-04",
+      "8. currency": "USD",
+      "9. matchScore": "0.5714",
+    },
+    {
+      "1. symbol": "BAAAAX",
+      "2. name": "Building America Strategy Port CDA USD Ser 21/1Q MNT CASH",
+      "3. type": "Mutual Fund",
+      "4. region": "United States",
+      "5. marketOpen": "09:30",
+      "6. marketClose": "16:00",
+      "7. timezone": "UTC-04",
+      "8. currency": "USD",
+      "9. matchScore": "0.5000",
+    },
+    {
+      "1. symbol": "BAAAFX",
+      "2. name": "Building America Strgy Portf CDA USD Ser 2022/2Q MNT CASH",
+      "3. type": "Mutual Fund",
+      "4. region": "United States",
+      "5. marketOpen": "09:30",
+      "6. marketClose": "16:00",
+      "7. timezone": "UTC-04",
+      "8. currency": "USD",
+      "9. matchScore": "0.5000",
+    },
+    {
+      "1. symbol": "BAB3.LON",
+      "2. name": "Leverage Shares 3x Alibaba ETP Securities",
+      "3. type": "ETF",
+      "4. region": "United Kingdom",
+      "5. marketOpen": "08:00",
+      "6. marketClose": "16:30",
+      "7. timezone": "UTC+01",
+      "8. currency": "USD",
+      "9. matchScore": "0.5000",
+    },
+    {
+      "1. symbol": "BAAX39.SAO",
+      "2. name": "Ishares Msci All Country Asia Ex Japan ETF",
+      "3. type": "ETF",
+      "4. region": "Brazil/Sao Paolo",
+      "5. marketOpen": "10:00",
+      "6. marketClose": "17:30",
+      "7. timezone": "UTC-03",
+      "8. currency": "BRL",
+      "9. matchScore": "0.3636",
+    },
+  ],
+};
 
+const SearchBar = () => {
   const [input, setInput] = useState("");
   const [filteredData, setFilteredData] = useState([]);
   const [showDropdown, setShowDropdown] = useState(false);
+  const useSampleData = false;
 
-  const handleInputChange = (event) => {
+  const handleInputChange = async (event) => {
     const inputValue = event.target.value.toUpperCase();
     setInput(inputValue);
+    console.log(inputValue);
 
     if (inputValue.length >= 2) {
-      const filteredResults = sampleData.bestMatches.filter((item) =>
-        item["1. symbol"].toUpperCase().startsWith(inputValue)
-      );
-      setFilteredData(filteredResults);
+      let results;
+
+      if (useSampleData) {
+        results = sampleData.bestMatches.filter((item) =>
+          item["1. symbol"].toUpperCase().startsWith(inputValue)
+        );
+      } else {
+        try {
+          const response = await fetch(
+            `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${inputValue}&apikey=G8GUQ1KETZSEZCOQ`
+          );
+          const data = await response.json();
+          console.log(data);
+          results = data.bestMatches;
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          return;
+        }
+      }
+
+      setFilteredData(results);
       setShowDropdown(true);
     } else {
       setFilteredData([]);
